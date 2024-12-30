60451935832d49442005b6ff8bbec57577367f45
EIQZhu <2753739236@qq.com>
2024-12-30 15:22:00 +0800

    MFA

---
+++ hello.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # 启用跨域支持
+
+@app.route('/user')
+def user():
+    return render_template('page1.html')
+    
+@app.route('/page2.html')
+def page2():
+    return render_template('page2.html')    
+
+
+@app.route('/page3.html')
+def page3():
+    return render_template('page3.html')    
+
+@app.route('/page4.html')
+def page4():
+    return render_template('page4.html')    
+    
+@app.route('/userdata', methods=['POST'])
+def login():
+    json = request.json
+    print('recv:', json)
+    return json
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    #,ssl_context=('server.crt','server.key'))
+
